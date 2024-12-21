7bbbea21e338e2fb93490a6f7d1e34bf64ea2fbd
LittleAnn <matuszyk.wet@gmail.com>
2024-12-21 23:23:41 +0100

    Counting charaters

---
+++ main.py
@@ -0,0 +1,50 @@
+def main():
+ print ('starting')
+ with open('books/frankenstein.txt') as f:
+        file_contents = f.read()
+        #print(file_contents)
+
+        ab = f'{file_contents}'
+        words = ab.split()
+        count = 0
+        for word in words:
+            count += 1
+        print (count)
+
+        lowered = file_contents.lower()
+        #print(lowered)
+
+        letter_count = {'a':0,
+                        'b':0,
+                        'c':0,
+                        'd':0,
+                        'e':0,
+                        'f':0,
+                        'g':0,
+                        'h':0,
+                        'i':0,
+                        'j':0,
+                        'k':0,
+                        'l':0,
+                        'm':0,
+                        'n':0,
+                        'o':0,
+                        'p':0,
+                        'r':0,
+                        's':0,
+                        't':0,
+                        'u':0,
+                        'w':0,
+                        'x':0,
+                        'y':0,
+                        'z':0}
+        
+        for letter in lowered:
+            if letter in letter_count:
+                letter_count[letter] +=1
+        return(letter_count)
+
+
+
+
+main()  
